88375a5e6ee715fc08bb4c08457a5ef219b21f77
Cristian795-cpu <cateran.23@est.ucab.edu.ve>
2024-09-22 11:35:14 -0400

    Add files via upload

--- Menú (proyecto zombies).cpp
+++ Menú (proyecto zombies).cpp
@@ -19,20 +19,19 @@ void escribir_archivo_jugador(ofstream &escribir){ /*La función crea el archivo
     cin>>nombre_jugador;
     cout<<"Ingrese el rango (lider / soldado): ";
     cin>>rango_jugador;
-    while(rango_jugador!="lider" || rango_jugador!="soldado"){
-        cout<<"El rango no coinside, vuelva a intentar."<<"\n";
-        cout<<"Ingrese el rango (lider / soldado): ";
-        cin>>rango_jugador;
-        if(rango_jugador=="lider" || rango_jugador=="soldado")
-            cin>>rango_jugador;
+    if(rango_jugador!="lider" && rango_jugador!="soldado"){
+        cout<<"Error, el dato no se encuentre entre las opciones\n"; /*prueba de modificacion para github*/
     }
-    
-    escribir.open("archivo jugador.txt", ios::out | ios::app);
-    escribir<<nombre_jugador<<" "<<rango_jugador<<" "<<salud_jugador<<" "<<afectacion_jugador<<"\n";
-    escribir.close();
+    else{
+        escribir.open("archivo jugador.txt", ios::out | ios::app);
+        escribir<<nombre_jugador<<" "<<rango_jugador<<" "<<salud_jugador<<" "<<afectacion_jugador<<"\n";
+        escribir.close();
+    }   
 }
 
+void leer_archivo_jugador(ifstream &leer){
 
+}
 
 int main(){
     ofstream escritura_jugador;
@@ -72,11 +71,11 @@ int main(){
                     cin>>opcion;
                     switch (opcion){
                         case 1:
-                            cout<<"falta programar"<<endl;
+                            cout<<"falta programar";
                             system("pause");
                         break;
                         case 2:
-                            cout<<"falta programar"<<endl;
+                            cout<<"falta programar";
                             system("pause");
                         break;
                         case 3:
@@ -109,7 +108,7 @@ int main(){
                                         system("pause");
                                         break;
                                     case 4:
-                                        cout<<"falta programar";
+                                        leer_archivo_jugador(lectura_jugador);
                                         system("pause");
                                         break;
                                 }
