acd0247bd38ac4dff7998206d1ca207ed6b3ecd6
akhira31 <168588311+akhira31@users.noreply.github.com>
2024-10-12 13:23:06 -0400

    inventario_de_arma.cpp

--- inventario_de_arma.cpp
+++ inventario_de_arma.cpp
@@ -27,26 +27,83 @@ void registrar_arma(PLista &lista) {
 
     cout << "\n\n\t\t[  REGISTRO  ]\n";
     cout << "\t\t------------";
-    cout << "\n\tSeleccione el arma a registrar:\n";
-    cout << "1. Bazuca\n";
-    cout << "2. Escopeta\n";
-    cout << "3. Cuchillo\n";
+    cout << "\n\tSeleccione el tipo de arma a registrar:\n";
+    cout << "1. Armas de fuego\n";
+    cout << "2. Armas arrojadizas\n";
+    cout << "3. Armas proyectiles\n";
+    cout << "4. Armas blancas\n";
+    cout << "5. Armas contundentes\n";
     cout << "Ingrese su opcion: ";
     cin >> opcion;
 
+    int subOpcion;
     switch (opcion) {
         case 1:
-            strcpy(q->nombrearm, "Bazuca");
+            cout << "Seleccione el arma de fuego:\n";
+            cout << "1. Pistola\n";
+            cout << "2. Escopeta\n";
+            cout << "3. Fusiles de asalto\n";
+            cout << "4. Rifles de francotirador\n";
+            cin >> subOpcion;
+            switch (subOpcion) {
+                case 1: strcpy(q->nombrearm, "Pistola"); break;
+                case 2: strcpy(q->nombrearm, "Escopeta"); break;
+                case 3: strcpy(q->nombrearm, "Fusiles de asalto"); break;
+                case 4: strcpy(q->nombrearm, "Rifles de francotirador"); break;
+                default: cout << "\nOpcion invalida."; delete q; return;
+            }
             break;
         case 2:
-            strcpy(q->nombrearm, "Escopeta");
+            cout << "Seleccione el arma arrojadiza:\n";
+            cout << "1. Granadas\n";
+            cout << "2. Cocteles molotov\n";
+            cin >> subOpcion;
+            switch (subOpcion) {
+                case 1: strcpy(q->nombrearm, "Granadas"); break;
+                case 2: strcpy(q->nombrearm, "Cocteles molotov"); break;
+                default: cout << "\nOpcion invalida."; delete q; return;
+            }
             break;
         case 3:
-            strcpy(q->nombrearm, "Cuchillo");
+            cout << "Seleccione el arma de proyectil:\n";
+            cout << "1. Ballestas\n";
+            cout << "2. Tira chinas\n";
+            cin >> subOpcion;
+            switch (subOpcion) {
+                case 1: strcpy(q->nombrearm, "Ballestas"); break;
+                case 2: strcpy(q->nombrearm, "Tira chinas"); break;
+                default: cout << "\nOpcion invalida."; delete q; return;
+            }
+            break;
+        case 4:
+            cout << "Seleccione el arma blanca:\n";
+            cout << "1. Cuchillos\n";
+            cout << "2. Machete\n";
+            cout << "3. Espada\n";
+            cin >> subOpcion;
+            switch (subOpcion) {
+                case 1: strcpy(q->nombrearm, "Cuchillos"); break;
+                case 2: strcpy(q->nombrearm, "Machete"); break;
+                case 3: strcpy(q->nombrearm, "Espada"); break;
+                default: cout << "\nOpcion invalida."; delete q; return;
+            }
+            break;
+        case 5:
+            cout << "Seleccione el arma contundente:\n";
+            cout << "1. Bat de beisbol\n";
+            cout << "2. Martillo\n";
+            cout << "3. Tuberia\n";
+            cin >> subOpcion;
+            switch (subOpcion) {
+                case 1: strcpy(q->nombrearm, "Bat de beisbol"); break;
+                case 2: strcpy(q->nombrearm, "Martillo"); break;
+                case 3: strcpy(q->nombrearm, "Tuberia"); break;
+                default: cout << "\nOpcion invalida."; delete q; return;
+            }
             break;
         default:
             cout << "\nOpcion invalida. No se registrara el arma.\n";
-            delete q; 
+            delete q;
             return;
     }
 
@@ -66,28 +123,84 @@ void registrar_arma(PLista &lista) {
 }
 /*------------------------ FUNCION ELIMINAR ARMA ---------------------*/
 void eliminar_arma(PLista &lista) {
-    int opcion;
+    int opcion, subOpcion;
     PLista q, t = nullptr;
     q = lista;
 
     cout << "\n\n\n\tELIMINAR UN ARMA";
-    cout << "\n\n\tSeleccione el arma a eliminar:\n";
-    cout << "1. Bazuca\n";
-    cout << "2. Escopeta\n";
-    cout << "3. Cuchillo\n";
+    cout << "\n\n\tSeleccione el tipo de arma a eliminar:\n";
+    cout << "1. Armas de fuego\n";
+    cout << "2. Armas arrojadizas\n";
+    cout << "3. Armas proyectiles\n";
+    cout << "4. Armas blancas\n";
+    cout << "5. Armas contundentes\n";
     cout << "Ingrese su opcion: ";
     cin >> opcion;
 
-    const char* nombreArma;
+    const char* nombreArma = nullptr;
     switch (opcion) {
         case 1:
-            nombreArma = "Bazuca";
+            cout << "Seleccione el arma de fuego:\n";
+            cout << "1. Pistola\n";
+            cout << "2. Escopeta\n";
+            cout << "3. Fusiles de asalto\n";
+            cout << "4. Rifles de francotirador\n";
+            cin >> subOpcion;
+            switch (subOpcion) {
+                case 1: nombreArma = "Pistola"; break;
+                case 2: nombreArma = "Escopeta"; break;
+                case 3: nombreArma = "Fusiles de asalto"; break;
+                case 4: nombreArma = "Rifles de francotirador"; break;
+                default: cout << "\nOpcion invalida."; return;
+            }
             break;
         case 2:
-            nombreArma = "Escopeta";
+            cout << "Seleccione el arma arrojadiza:\n";
+            cout << "1. Granadas\n";
+            cout << "2. Cocteles molotov\n";
+            cin >> subOpcion;
+            switch (subOpcion) {
+                case 1: nombreArma = "Granadas"; break;
+                case 2: nombreArma = "Cocteles molotov"; break;
+                default: cout << "\nOpcion invalida."; return;
+            }
             break;
         case 3:
-            nombreArma = "Cuchillo";
+            cout << "Seleccione el arma de proyectil:\n";
+            cout << "1. Ballestas\n";
+            cout << "2. Tira chinas\n";
+            cin >> subOpcion;
+            switch (subOpcion) {
+                case 1: nombreArma = "Ballestas"; break;
+                case 2: nombreArma = "Tira chinas"; break;
+                default: cout << "\nOpcion invalida."; return;
+            }
+            break;
+        case 4:
+            cout << "Seleccione el arma blanca:\n";
+            cout << "1. Cuchillos\n";
+            cout << "2. Machete\n";
+            cout << "3. Espada\n";
+            cin >> subOpcion;
+            switch (subOpcion) {
+                case 1: nombreArma = "Cuchillos"; break;
+                case 2: nombreArma = "Machete"; break;
+                case 3: nombreArma = "Espada"; break;
+                default: cout << "\nOpcion invalida."; return;
+            }
+            break;
+        case 5:
+            cout << "Seleccione el arma contundente:\n";
+            cout << "1. Bat de beisbol\n";
+            cout << "2. Martillo\n";
+            cout << "3. Tuberia\n";
+            cin >> subOpcion;
+            switch (subOpcion) {
+                case 1: nombreArma = "Bat de beisbol"; break;
+                case 2: nombreArma = "Martillo"; break;
+                case 3: nombreArma = "Tuberia"; break;
+                default: cout << "\nOpcion invalida."; return;
+            }
             break;
         default:
             cout << "\nOpcion invalida. No se eliminara el arma.\n";
@@ -163,6 +276,6 @@ int main(void) {
         system("cls");
 
     } while (opcion != 4);
-    
+
     return 0;
 }
