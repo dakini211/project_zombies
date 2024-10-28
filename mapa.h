6bdf0da2d6de4cc8a1d72f849f7e81156951638b
Lawliet-cpu <cateran.23@est.ucab.edu.ve>
2024-10-27 21:13:19 -0400

    mapa.h

--- mapa.h
+++ mapa.h
@@ -108,60 +108,63 @@ mapa *devolver_estacion_mapa(mapa*lista_grupo,string nombre){
     }
 }
 
-void existe_estacion(mapa *lista, mapa *auxiliar, string valor_1){
-    mapa *buscar_2=lista;
-    bool encontrado_2=false;
-    while(lista_vacia_mapa(buscar_2) && encontrado_2==false){
-        if(buscar_2->nombre_estacion==valor_1){
-            encontrado_2=true;
-            cout<<"\e[47m \e[0m";
-            cout<<"\e[41mEsta estacion ya existe\e[0m";
-            cout<<"\e[47m \e[0m\n\n"; 
+void existe_estacion(mapa *lista, string valor_1) {
+    mapa *buscar_2 = lista;
+    bool encontrado_2 = false;
+
+    while (buscar_2 != NULL && !encontrado_2) {
+        if (buscar_2->nombre_estacion == valor_1) {
+            encontrado_2 = true;
+            cout << "\e[47m \e[0m";
+            cout << "\e[41mEsta estacion ya existe\e[0m";
+            cout << "\e[47m \e[0m\n\n";
         }
-        else{
-            buscar_2=buscar_2->proximo;
-        }
-        if(lista_vacia_mapa(buscar_2)){
-            auxiliar->nombre_estacion= valor_1;
-            cout<<"\n";
-            cout<<"\e[47m \e[0m";
-            cout<<"\e[42mEl jugador se modifico con exito!!!\e[0m";   
-            cout<<"\e[47m \e[0m\n\n";      
-        }
+        buscar_2 = buscar_2->proximo;
+    }
+
+    if (!encontrado_2) {
+        cout << "\n";
+        cout << "\e[47m \e[0m";
+        cout << "\e[42mEl nombre de la estacion se modificó con éxito!!!\e[0m";
+        cout << "\e[47m \e[0m\n\n";
     }
 }
 
-void buscar_estacion(mapa *list, string actual){
-    mapa *buscar = list;                      
-    bool encontrado = false; 
-    if (lista_vacia_mapa(list)){
-        cout<<"\e[47m \e[0m";
-        cout<<"\e[41mNo se han introducido datos del jugador\e[0m";
-        cout<<"\e[47m \e[0m\n\n";
-    }  
-    else{   
-        while (!lista_vacia_mapa(buscar) && encontrado == false) {
-            if (buscar->nombre_estacion == actual){
+void buscar_estacion(mapa *list, string actual) {
+    mapa *buscar = list;
+    bool encontrado = false;
+
+    if (lista_vacia_mapa(list)) {
+        cout << "\e[47m \e[0m";
+        cout << "\e[41mNo se han introducido datos de la estacion\e[0m";
+        cout << "\e[47m \e[0m\n\n";
+    } else {
+        while (buscar != NULL && !encontrado) {
+            if (buscar->nombre_estacion == actual) {
                 encontrado = true;
                 string nuevo_nombre;
-                cout<<"Ingrese el nuevo nombre de la estacion: ";
-                cin>>nuevo_nombre; 
-                existe_estacion(list, buscar, nuevo_nombre);
-            }
-            else{
+                cout << "Ingrese el nuevo nombre de la estacion: ";
+                cin >> nuevo_nombre;
+                existe_estacion(list, nuevo_nombre);
+                buscar->nombre_estacion = nuevo_nombre;
+            } 
+            else {
                 buscar = buscar->proximo;
             }
         }
-        if (!encontrado){
-            cout<<"\e[47m \e[0m";
-        cout<<"\e[41mEl jugador no fue encontrado en la lista\e[0m";
-            cout<<"\e[47m \e[0m\n\n";
+        if (!encontrado) {
+            cout << "\e[47m \e[0m";
+            cout << "\e[41mLa estacion no fue encontrada en la lista\e[0m";
+            cout << "\e[47m \e[0m\n\n";
         }
     }
 }
 
-void modificar_estacion(mapa *inicio){ 
-    buscar_estacion(inicio, pedir_dato_estacion());
+void modificar_estacion(mapa *inicio) { 
+    string nombre_a_modificar;
+    cout << "Ingrese el nombre de la estacion que desea modificar: ";
+    cin >> nombre_a_modificar;
+    buscar_estacion(inicio, nombre_a_modificar);
 }
 
 void agregar_zombies_mapa(zombies *list_zombie, mapa *&list_mapa){
@@ -218,7 +221,7 @@ void mostrar_mapa(mapa *list){
         mover_estacion = list;
         while (mover_estacion != NULL){
             cout <<"\n\n\e[47m[Estacion "<<mover_estacion->nombre_estacion <<"]\e[0m\n";  
-            cout<<"******************************************************\n"; 
+            cout<<"******************************************************************************\n"; 
             mover_zombies = mover_estacion->zombies_estacion;  
             if (mover_zombies != NULL){
                 while(mover_zombies!=NULL){
@@ -229,7 +232,7 @@ void mostrar_mapa(mapa *list){
             else{
                 cout<<endl<<"La estacion aun no tiene zombies"<<endl;
             }
-           cout<<"******************************************************\n";   
+           cout<<"******************************************************************************\n";   
            mover_estacion = mover_estacion->proximo;
         }   
     }
@@ -238,56 +241,43 @@ void mostrar_mapa(mapa *list){
     }
 }
 
-void modificar_zomb(zombies *list, zombies *lista){
-    zombies *mover=list;
-    while(mover!=NULL){
-        if(mover->nivel== lista->nivel){
-            mover->nombre=lista->nombre;
-            mover->nivel=lista->nivel;
-            mover->salud=lista->salud;
-            mover->dano_ataque=lista->dano_ataque;
+void modificar_zomb(zombies *list, zombies *nueva_info) {
+    zombies *mover = list;
+    while (mover != NULL) {
+        if (mover->nivel == nueva_info->nivel) {
+            mover->nombre = nueva_info->nombre;
+            mover->nivel = nueva_info->nivel;
+            mover->salud = nueva_info->salud;
+            mover->dano_ataque = nueva_info->dano_ataque;
         }
-        mover=mover->prox;
+        mover = mover->prox;
     }
 }
 
-void modificar_tipo_zombi(mapa *lista, zombies *lista_zomb){
-    mapa *ubicacion=NULL;
-    zombies *lista_zombies_mapa=NULL;
-    int numero_zombies_estacion;
-    int numero_zombies;
-    string estacion;
-    cout<<"Ingrese el nombre de la estacion a modificas tipos de zombies: ";
-    cin>>estacion;
-    ubicacion=devolver_estacion_mapa(lista, estacion);
+void eliminar_zombie_mapa(mapa *&lista){
     
-    if(ubicacion==NULL){
-        cout<<"No hay estaciones disponibles"<<endl;
-    }
-    else{
-        zombies *buscar_nivel_zombie=NULL;
-        cout<<"Indica nivel de peligro del zombies a modificar: ";
-        cin>>numero_zombies_estacion;
-        buscar_nivel_zombie=buscar_zombie_para_mapa(ubicacion->zombies_estacion, numero_zombies_estacion);
-        if((numero_zombies_estacion!=buscar_nivel_zombie->nivel) && (numero_zombies==0)){
-            cout<<"No hay zombies con ese nivel en la lista\n";
-        }
-
-        else{
-            cout<<"Indica nuevo nivel de peligro de zombie a reemplazar: ";
-            cin>>numero_zombies_estacion;
-            zombies *modificar= buscar_zombie_para_mapa(lista_zomb, numero_zombies_estacion);
-            modificar_zomb(ubicacion->zombies_estacion, modificar);
-
-            if(numero_zombies_estacion<0){
-                cout<<"No puedes pedir un numero negativo"<<endl;
-            }
-
-            if(numero_zombies_estacion>numero_zombies){
-                cout<<"No puedes pedir un numero mayor a la cantidad de zombies\n";
-            }
-        }
+    while(lista->zombies_estacion!=NULL){
+        zombies *aux=lista->zombies_estacion;
+        lista->zombies_estacion=aux->prox;
+        delete aux;
     }
 }
+
+void modificar_zombi(mapa *&lista) {
+    mapa *ubicacion = NULL;
+    string estacion;
+
+    cout << "Ingrese el nombre de la estacion a eliminar zombies: ";
+    cin >> estacion;
+    ubicacion = devolver_estacion_mapa(lista, estacion);
+    
+    if (ubicacion == NULL) {
+        cout << "No hay estaciones disponibles" << endl;
+        return;
+    }
+
+    eliminar_zombie_mapa(ubicacion);
+}
+
 
 #endif 
